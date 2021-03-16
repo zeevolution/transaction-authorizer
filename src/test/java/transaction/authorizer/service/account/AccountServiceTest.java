11741c2413a78637ebd6047e18db91f6163f7ff3
zeevolution <limajosent@gmail.com>
2021-03-15 23:37:45 -0300

    test: add account service unit test

---
+++ src/test/java/transaction/authorizer/service/account/AccountServiceTest.java
@@ -0,0 +1,67 @@
+package transaction.authorizer.service.account;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class AccountServiceTest {
+
+    @InjectMocks
+    private AccountService accountService;
+
+    @Test
+    public void processActiveAccountTest() {
+        // Arrange
+        final boolean activeCard = true;
+        final int availableLimit = 100;
+        final String expectedOutput = "{\"account\": {\"active-card\": "+ activeCard +", \"available-limit\": " +
+                + availableLimit + "}, \"violations\": []}\n";
+
+        // Act
+        final String actualOutput = accountService.processAccount(activeCard, availableLimit);
+
+        // Assert
+        Assertions.assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void processInactiveAccountTest() {
+        // Arrange
+        final boolean activeCard = false;
+        final int availableLimit = 100;
+        final String expectedOutput = "{\"account\": {\"active-card\": "+ activeCard +", \"available-limit\": " +
+                + availableLimit + "}, \"violations\": []}\n";
+
+        // Act
+        final String actualOutput = accountService.processAccount(activeCard, availableLimit);
+
+        // Assert
+        Assertions.assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void processMultipleAccountsTest() {
+        // Arrange
+        final boolean activeCard = true;
+        final int availableLimit = 100;
+        final String expectedFirstOutput = "{\"account\": {\"active-card\": "+ activeCard +", \"available-limit\": " +
+                + availableLimit + "}, \"violations\": []}\n";
+        final String expectedSecondOutput = "{\"account\": {\"active-card\": "+ activeCard +", \"available-limit\": " +
+                + availableLimit + "}, \"violations\": [account-already-initialized]}\n";
+        final String expectedThirdOutput = "{\"account\": {\"active-card\": "+ activeCard +", \"available-limit\": " +
+                + availableLimit + "}, \"violations\": [account-already-initialized]}\n";
+
+        // Act
+        final String actualFirstOutput = accountService.processAccount(activeCard, availableLimit);
+        final String actualSecondOutput = accountService.processAccount(activeCard, availableLimit);
+        final String actualThirdOutput = accountService.processAccount(activeCard, availableLimit);
+
+        // Assert
+        Assertions.assertEquals(expectedFirstOutput, actualFirstOutput);
+        Assertions.assertEquals(expectedSecondOutput, actualSecondOutput);
+        Assertions.assertEquals(expectedThirdOutput, actualThirdOutput);
+    }
+}
